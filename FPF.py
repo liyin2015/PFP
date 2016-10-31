82491e869f19915f9ddf2dc41e9adfaea22644d7
liyin2015 <liyin199102@gmail.com>
2016-10-30 23:09:00 -0500

    add readme
    add Readme

--- FPF.py
+++ FPF.py
@@ -3,17 +3,20 @@ from pyspark import SparkContext, SparkConf
 import csv
 
 conf = SparkConf().setAppName("FPF")
+#conf = conf.setMaster("local[*]")
 sc = SparkContext(conf=conf)
+
+sc.setLogLevel("ERROR")
 #sc = SparkContext
-data = sc.textFile("/Users/yinli/software/spark-2.0.1-bin-hadoop2.7/data/mllib/sample_fpgrowth_copy.txt")
+data = sc.textFile("/Users/yinli/Downloads/fpgrowth-master/5000-out1.txt")
 #data = sc.textFile("/Users/yinli/Downloads/fpgrowth-master/5000-out1.csv");
 #data = sc.textFile("/Users/yinli/Downloads/fpgrowth-master/5000-out1.csv").map(lambda line: line.split(",")).filter(lambda line: len(line)>1).map(lambda line: (line[0],line[1])).collect()
-transactions = data.map(lambda line: line.strip().split(','))
-print transactions
-for fi in transactions.collect():
-    del transactions.collect()[0]
-    #print(fi)
-model = FPGrowth.train(transactions, minSupport=0.2, numPartitions=1)
+transactions = data.map(lambda line: line.strip().split('\t'))
+#print transactions
+#for fi in transactions.collect():
+ #   del fi[0]
+  #  print(fi)
+model = FPGrowth.train(transactions, minSupport=0.01, numPartitions=2)
 result = model.freqItemsets().collect()
 for fi in result:
     print(fi)
---
+++ readMe.txt
@@ -0,0 +1,3 @@
+time /Users/yinli/software/spark-2.0.1-bin-hadoop2.7/bin/spark-submit FPF.py
+
+this command is used to submit and run spark program
